255febecc032b59fb6be326c6a8b09e938a186bd
Joey Schluchter <jschluchter@gmail.com>
2017-12-22 22:15:59 -0500

    update home layout

--- rating-web/config/dev.env.js
+++ rating-web/config/dev.env.js
@@ -1,11 +1,12 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
+const siteUrl = '"http://localhost:8080/"'
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   SITE_DATA: { 
-    home: '"http://localhost:8080/static/home.json"',
-    rating: '"http://localhost:8080/static/rating.json"',
-    leaderboard: '"http://localhost:8080/static/leaderboard.json"'
+    home: '"http://localhost:8080/static/data/home.json"',
+    rating: '"http://localhost:8080/static/data/rating.json"',
+    leaderboard: '"http://localhost:8080/static/data/leaderboard.json"'
   }
 })
--- rating-web/src/components/Home.vue
+++ rating-web/src/components/Home.vue
@@ -1,32 +1,68 @@
 <template>
-  <b-jumbotron :header="content.title" :lead="content.subtitle" >
-    <p>{{content.info}}</p>
-    <p>Check out the source on <a :href="content.github" target="_blank">github</a>
+  <div>
+    <b-jumbotron>
+      <p>
+        <img :src="content.headerImage" width="250px" />
+      </p>
+      <p>
+        <h6>{{content.subtitle}}</h6>
+      </p>
+      <p>
+        <b-btn variant="success" :to="'Rating'">&nbsp;{{content.rating}}&nbsp;<i class="fa fa-md fa-heart"></i></b-btn>
+        <b-btn variant="primary" :to="'Leaderboard'">{{content.leaderboard}}&nbsp;<i class="fa fa-md fa-dashboard"></i></b-btn>
+      </p>
+    </b-jumbotron>
+    <h6>{{content.createInfo}}</h6>
+    <p>
+      <img :src="content.logoImage" />
+      <span class="largeWhite">+&nbsp;</span>
+      <img :src="content.azureLogo" />
     </p>
-    <b-btn variant="success" :to="'Rating'">{{content.rating}}</b-btn>
-    <b-btn variant="primary" :to="'Leaderboard'">{{content.leaderboard}}</b-btn>
-  </b-jumbotron>
+    <h6>source available on</h6>
+    <p>
+      <a :href="content.githubRepo" target="_blank">
+        <i class="fa fa-github fa-2x fa-inverse"></i>
+      </a>
+    </p>
+  </div>
 </template>
 
 <script>
-const site = process.env.SITE_DATA
-import axios from 'axios'
+  const site = process.env.SITE_DATA
+  import axios from 'axios'
 
-export default {
-  data () {
-    return {
-      content: [],
-      errors: []
+  export default {
+    data () {
+      return {
+        content: [],
+        errors: []
+      }
+    },
+    created () {
+      axios
+        .get(site.home)
+        .then(response => {
+          this.content = response.data
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
     }
-  },
-  created () {
-    axios.get(site.home)
-    .then(response => {
-      this.content = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
   }
-}
 </script>
+<style>
+  body {
+    background-color: #444;
+    color: #fff;
+  }
+  h6{
+    color: #fff;
+  }
+  .largeWhite{
+    color: #fff;
+  }
+  .jumbotron {
+    background-color: #777;
+    color: #fff;
+  }
+</style>
